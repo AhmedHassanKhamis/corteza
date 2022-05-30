1b1e095c81fd6f79aaee20eb7c6bab3af20cb6d4
ahmedhassank <92717109+ahmedhassank@users.noreply.github.com>
2022-05-30 21:59:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mkdir corteza/data/db
 mkdir corteza/data/server
 touch corteza/docker-compose.yaml
 cd corteza
-sudo chown 1001:1001 data/db
-sudo chown 4242:4242 data/server
+chown 1001:1001 data/db
+chown 4242:4242 data/server
 
