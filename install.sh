19fe302d6099620b9a94cb9643d7355781d44df6
ahmedhassank <92717109+ahmedhassank@users.noreply.github.com>
2022-05-30 22:00:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-mkdir corteza
-mkdir corteza/data
-mkdir corteza/data/db
-mkdir corteza/data/server
-touch corteza/docker-compose.yaml
-cd corteza
 chown 1001:1001 data/db
 chown 4242:4242 data/server
 
